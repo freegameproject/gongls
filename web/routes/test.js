a484ce950eec5906108c190b85b6af58797dd8bb
freegameproject <freegameproject@sina.com>
2016-01-18 12:16:12 +0800

    up

--- web/routes/test.js
+++ web/routes/test.js
@@ -10,4 +10,8 @@ router.get('/1', function(req, res, next) {
   res.render('test/1',{title:'测你在哪个阶层生活能如鱼得水？'});
 });
 
+router.get('/2', function(req, res, next) {
+  res.render('test/2',{title:'你第一眼看到哪处不同？准到爆！'});
+});
+
 module.exports = router;
