96470cc8a3b49c9f47f2926b554d555e063b54d4
freegameproject <freegameproject@sina.com>
2016-01-29 11:51:06 +0800

    ...

--- web/routes/index.js
+++ web/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'gongls' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'login' });
+});
+
+router.get('/reg', function(req, res, next) {
+  res.render('reg', { title: 'reg' });
+});
+
+
+router.get('/iphone', function(req, res, next) {
+  res.render('iphone', { title: 'reg' });
+});
+
 module.exports = router;
